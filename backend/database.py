8f33288716a0f4cf29eaefc4c359c99953c91e05
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-05-29 17:37:02 -0400

    Initial setup with postgresql and fastapi

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ fastapi_tutorial.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+'''
+fastapi course for beginners by FreeCodeCamp.org
+https://www.youtube.com/watch?v=tLKKmouUams
+
+Python Object Oriented Programming (OOP) - For Beginners
+https://www.youtube.com/watch?v=JeznW_7DlB0
+'''
