3cb35ea8b36042a17c84ea45815ac798496c3f98
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-06-10 18:20:16 -0400

    add CORS middleware and register calculate router

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+fomr Fastapi.middleware.cors import CORSMiddleware
 from .database import engine, Base
 from .models.debt import Debt
+from .routers import calculate
 
 app = FastAPI()
 
+#Create CORS middleware to allow requests from the frontend
 Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_oringins=["*"],
+    allow_credentials=True,
+    allow_method=["*"],
+    allow_headers=["*"],
+)
+
+#Register the calculate router
+app.include_router(calculate.router)
 
 @app.get("/")
 def read_root():
