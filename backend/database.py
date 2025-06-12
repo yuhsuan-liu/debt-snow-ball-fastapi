4e530952d01376e2a9501407f2c66b31526a90e6
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-06-12 15:05:13 -0400

    Add DATABASE_URL for SQLite before initiating database; Create a CRUD operations file for users and a new router for user operations

--- backend/database.py
+++ backend/database.py
@@ -13,3 +13,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
