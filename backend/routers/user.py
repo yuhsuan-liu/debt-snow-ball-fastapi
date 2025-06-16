9cbb23f4d9ab10427de5bf1546020c64937520d8
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-06-16 18:49:02 -0400

    migrate to PostgreSQL: update DB URL, load from .env, switch to Numeric types

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -12,7 +12,7 @@ router = APIRouter()
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = user_crud.get_user_by_username(db, username=user.username)
     if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(status_code=400, detail="Username already in use, please choose another username")
     return user_crud.create_user(db=db, user=user)
 
 @router.get("/users/{username}", response_model=User)
