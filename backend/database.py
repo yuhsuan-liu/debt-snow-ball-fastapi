40dccaf7299e6f00d43b76a742c702c33ea8ef96
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-06-16 19:19:08 -0400

    Dropped all the old tables created with SQLite and initiate PostgreSQL schema

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,8 @@ import os
 
 # Load environment variables
 load_dotenv(override=True)
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL="postgresql://snowball_db_user:HAgSDx9zc8i9Mbprx6nHeZh8QDYHPJT5@dpg-d189qbmuk2gs73fmvqs0-a.virginia-postgres.render.com/snowball_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
