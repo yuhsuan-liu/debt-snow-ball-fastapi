b92faedd57678011abe624abf781c1892e6fcc65
YuhsuanLiu <yuhsuan.career@gmail.com>
2025-08-04 17:05:26 -0400

    Add detailed comments to main.py

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,10 @@ from backend.models.debt import Debt
 from backend.routers import calculate, user, payment_plan
 from sqlalchemy.orm import Session
 
+#Use FastAPI to initiate the main application
 app = FastAPI()
 
-# Allow CORS for the frontend application
+# Allow CORS for the frontend application to send requests to the backend
 origin = [
     "http://localhost:5173",
     #Insert github page URL here once deployed 
@@ -32,6 +33,7 @@ app.include_router(calculate.router)
 app.include_router(user.router, tags=["users"])
 app.include_router(payment_plan.router, tags=["payment-plans"])
 
+# Define a root endpoint to check if the backend is running
 @app.get("/")
 def read_root():
     return {"message": "DebtSnowball backend is live"}
